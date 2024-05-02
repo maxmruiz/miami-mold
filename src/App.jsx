e62a30491c4de4a83af84dc40c599f6c7bd77743
maxmruiz <mmfern2003@gmail.com>
2024-05-02 13:41:34 -0400

    skeleton of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
+import OurWork from './assets/components/OurWork'
+import Spanish from './assets/components/Spanish'
 
 function App() {
 
   return (
     <>
-      
+      <div className="header">
+            <img src="" alt="Miami Mold Logo"/>
+            <Spanish />
+            <OurWork />
+        </div>
+        <div className="aboutUs">
+            <div className="about">
+
+            </div>
+            <div className="services">
+
+            </div>
+        </div>
+        <div className="contactUs">
+            <div className="email">
+
+            </div>
+            <div className="phone">
+                
+            </div>
+        </div>
     </>
   )
 }
